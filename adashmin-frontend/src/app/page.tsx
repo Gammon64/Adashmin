95d158d81b13ada324fdab6cdbba47df5280c126
Hugo <hugo.hbs64@gmail.com>
2024-05-16 20:34:17 -0300

    Ajustes iniciais CSS

--- adashmin-frontend/src/app/page.tsx
+++ adashmin-frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
+import style from "./page.module.css";
+
 export default function Home() {
   return (
-    <main>
+    <main className={style.main}>
       <h1>Adashmin</h1>
       <p>Gerenciamento de funcionários</p>
     </main>
