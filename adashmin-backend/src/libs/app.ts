d14fb610a3785fec4fef63362ab4479c8fdf0ab9
Hugo <hugo.hbs64@gmail.com>
2024-05-16 18:21:48 -0300

    Rotas para funcionário

--- adashmin-backend/src/libs/app.ts
+++ adashmin-backend/src/libs/app.ts
@@ -2,10 +2,15 @@
  * Configura o servidor Express.js
  */
 import express from "express";
+import funcionario_route from "../routes/funcionario";
+import bodyParser from "body-parser";
 const app = express();
 
+app.use(bodyParser.json());
+app.use("/", funcionario_route);
+
 app.get("/", (req, res) => {
-  res.send("Boas vindas à API Adasmin!");
+  res.send("Boas vindas à API Adashmin!");
 });
 
 export default app;
---
+++ adashmin-backend/src/routes/funcionario.ts
@@ -0,0 +1,66 @@
+import { Router } from "express";
+import FuncionarioModel from "../model/funcionario";
+
+/**
+ * Endpoints para operaćões CRUD de funcionários
+ */
+const funcionario_route = Router();
+
+funcionario_route.get("/employees", async (req, res) => {
+  try {
+    const funcionarios = await FuncionarioModel.find();
+    res.send(funcionarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao buscar funcionários");
+  }
+});
+
+funcionario_route.get("/employees/:id", async (req, res) => {
+  try {
+    const funcionario = await FuncionarioModel.findById(req.params.id);
+    res.send(funcionario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao buscar funcionário");
+  }
+});
+
+funcionario_route.post("/employees", async (req, res) => {
+  try {
+    // Pega o body da requisição e cria um novo funcionário
+    console.log("🚀 ~ funcionario_route.post ~ req.body:", req.body);
+    const funcionario = new FuncionarioModel(req.body);
+    await funcionario.save();
+    res.send(funcionario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao criar funcionário");
+  }
+});
+
+funcionario_route.put("/employees/:id", async (req, res) => {
+  try {
+    const funcionario = await FuncionarioModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.send(funcionario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao atualizar funcionário");
+  }
+});
+
+funcionario_route.delete("/employees/:id", async (req, res) => {
+  try {
+    await FuncionarioModel.findByIdAndDelete(req.params.id);
+    res.send("Funcionário excluído com sucesso");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao excluir funcionário");
+  }
+});
+
+export default funcionario_route;
