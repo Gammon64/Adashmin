47ab4441ad273f1dd1d1b6ab265135ea964748fe
Hugo <hugo.hbs64@gmail.com>
2024-05-16 22:53:41 -0300

    Toast ao salvar

--- adashmin-frontend/src/app/_components/EmployeeForm.tsx
+++ adashmin-frontend/src/app/_components/EmployeeForm.tsx
@@ -3,8 +3,10 @@ import { Button, Input } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Funcionario } from "./ListEmployee";
+import { useToast } from "@chakra-ui/react";
 
 const EmployeeForm = ({ funcionario }: { funcionario?: Funcionario }) => {
+  const toast = useToast();
   const method = useForm();
 
   useEffect(() => {
@@ -13,23 +15,31 @@ const EmployeeForm = ({ funcionario }: { funcionario?: Funcionario }) => {
     }
   }, [funcionario]);
 
+  /**
+   * Envia o funcionário para o backend
+   */
   const onSave = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     method.handleSubmit((data) => {
-      fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/employees/${
-          funcionario?._id ?? ""
-        }`,
+      toast.promise(
+        fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/employees/${
+            funcionario?._id ?? ""
+          }`,
+          {
+            method: "POST",
+            body: JSON.stringify(data),
+          }
+        ),
         {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
+          success: { title: "Sucesso!", description: "Funcionário salvo" },
+          error: { title: "Erro!", description: "Funcionário não foi salvo" },
+          loading: { title: "Enviando...", description: "Aguarde um momento" },
         }
       );
-    });
+    })();
   };
+
   return (
     <form
       onSubmit={onSave}
@@ -64,7 +74,9 @@ const EmployeeForm = ({ funcionario }: { funcionario?: Funcionario }) => {
         })}
         type="datetime-local"
       />
-      <Button variant="ghost">Salvar</Button>
+      <Button variant="ghost" type="submit">
+        Salvar
+      </Button>
     </form>
   );
 };
