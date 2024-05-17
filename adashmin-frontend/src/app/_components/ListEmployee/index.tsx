f432132f520829301fe74f06a195d6221b7d2027
Hugo <hugo.hbs64@gmail.com>
2024-05-17 17:59:39 -0300

    Ajustes para evitar cache ao excluir registro

--- adashmin-frontend/src/app/_components/ListEmployee/index.tsx
+++ adashmin-frontend/src/app/_components/ListEmployee/index.tsx
@@ -1,31 +1,8 @@
+import { buscar } from "@/app/funcionario/actions";
 import DataTable from "./DataTable";
 
-/**
- * Lista todos os funcionários.
- * Caso haja query, lista todos os funcionários que contém a query.
- * @param query valor a ser filtrado
- * @returns
- */
-async function getData(query: string | undefined) {
-  // Faz a requisição para o backend, passando a query como parâmetro se existir
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/employees${
-      query ? "?query=" + query : ""
-    }`,
-    {
-      method: "GET",
-    }
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 const ListEmployee = async ({ query }: { query: string | undefined }) => {
-  const data = await getData(query);
+  const data = await buscar(query);
   return <DataTable data={data} />;
 };
 
--- adashmin-frontend/src/app/funcionario/actions.tsx
+++ adashmin-frontend/src/app/funcionario/actions.tsx
@@ -2,6 +2,30 @@
 
 import { revalidatePath } from "next/cache";
 
+/**
+ * Lista todos os funcionários.
+ * Caso haja query, lista todos os funcionários que contém a query.
+ * @param query valor a ser filtrado
+ * @returns
+ */
+export const buscar = async (query?: string) => {
+  // Faz a requisição para o backend, passando a query como parâmetro se existir
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/employees${
+      query ? "?query=" + query : ""
+    }`,
+    {
+      method: "GET",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
 export const salvar = async (data: any, id?: string) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BACKEND_URL}/employees/${id ?? ""}`,
