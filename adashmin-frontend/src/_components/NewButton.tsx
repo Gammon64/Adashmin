e5536298193aeeb6837531fa53910da1505bafa2
Hugo <hugo.hbs64@gmail.com>
2024-09-11 19:30:07 -0300

    Botão novo Server Side

--- adashmin-frontend/src/_components/NewButton.tsx
+++ adashmin-frontend/src/_components/NewButton.tsx
@@ -1,14 +1,20 @@
-"use client";
-import { AddIcon } from "@chakra-ui/icons";
-import { Link } from "@chakra-ui/next-js";
-import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 
 const NewButton = () => {
   return (
     <Link href="funcionario">
-      <Button colorScheme="purple" leftIcon={<AddIcon />}>
+      <button
+        style={{
+          backgroundColor: "#007070",
+          padding: "0.7rem 1.5rem",
+          border: "none",
+          borderRadius: "0.5rem",
+          color: "white",
+          fontWeight: "bold",
+        }}
+      >
         Novo
-      </Button>
+      </button>
     </Link>
   );
 };
